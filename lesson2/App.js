2fe600b012c25188fbc83ad75122cf8d4988067b
MBE10 <165320368+MBE10@users.noreply.github.com>
2026-01-15 17:46:35 +0100

    .

---
+++ lesson2/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+importt MainScreen from './screens/MainScreen';
+
+const stack = createStackNavigator();
+
+export default function App() {
+  return (
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MainScreen} />
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ lesson2/screens/MainScreen.js
@@ -0,0 +1,24 @@
+import { setStatusBarBackgroundColor } from "expo-status-bar";
+import React from "react";
+import { Text, Stylesheet, View  } from "react-native";
+
+const MainScreen = () => {
+
+    return
+    <view style={style.container}>
+        <text style={style.TextStyle}>This is Main MainScreen</text>
+    </view>
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    }
+    TextStyle: {
+        fontSize: 30
+    }
+});
+
+export defeault MainScreen;
