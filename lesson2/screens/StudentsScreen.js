d3a8275f719eb715230bf9e376f650d5064cf478
MBE10 <165320368+MBE10@users.noreply.github.com>
2026-02-10 17:45:25 +0100

    .

--- lesson2/screens/StudentsScreen.js
+++ lesson2/screens/StudentsScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Text,View,StyleSheet} from 'react-native';
+import StudentDetails from '../components/StudentDetails';
 
 const TestScreen = () => {
     return( 
     <View>
 
         <Text style={styles.textStyle}> Students Screen </Text>;
+        <StudentDetails name="Ana" description="Lorem ipsum"/>
+        <StudentDetails name="Leo" description="Lorem ipsum"/>
+        <StudentDetails name="Anita" description="Lorem ipsum" />
     
     </View>    
 
